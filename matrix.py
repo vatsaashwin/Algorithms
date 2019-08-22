4d7627faa7756d7db9051bbd6c485b137eb73fb9
vatsaashwin <aashwinvats@GMAIL.com>
2019-08-22 12:23:46 -0700

    stack implementation

--- matrix.py
+++ matrix.py
@@ -2,18 +2,21 @@
 arr = [[0,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
 print(arr)
 
-def changeVal(arr, r, c):
+def changeVal(arr):
 	rows= len(arr)
 	cols = len(arr[0])
 
-	if arr[r][c] == 0:
-		for i in range(rows):
-
-			arr[i][c]=0
-			for j in range(cols):
-				arr[r][j]=0
-
+	# if arr[][c] == 0:
+		
+	for i in range(rows):
+		print(i)
+		for j in range(cols):
+			print("----------", j)
+			if arr[i][j]==0:
+				r = i
+				c = j
+			
+		
 	print(arr)
 
-
-changeVal(arr, 0, 0)
+changeVal(arr)
